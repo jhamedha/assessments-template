d3031c4f6d3536eff61980a8701cbb43d15b9def
Medha Jha <medha.r.jha@gmail.com>
2021-10-04 18:37:58 -0500

    initial render of table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+
+type country = {
+  name: string,
+  topLevelDomain: string[],
+  alpha2Code: string,
+  alpha3Code: string,
+  callingCodes: string[],
+  capital: string,
+  altSpellings: string[],
+  region: string
+}
+
+const ProductTable: React.FunctionComponent<any> = props => {
+  return (
+    <table>
+      <caption>Country Data</caption>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Capital</th>
+          <th>Region</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.countryData.map((prop: country) => (
+          <tr key={prop.alpha2Code}>
+            <td>{prop.name}</td>
+            <td>{prop.capital}</td>
+            <td>{prop.region}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
 
 function App() {
-  return <div>Insert your code here...</div>;
+  const [countryData, setCountryData] = useState([]);
+
+  useEffect(() => {
+    const fetchCountryData = async() => {
+      await fetch('http://api.countrylayer.com/v2/all?access_key=xx')
+        .then(results => results.json())
+        .then(data => {
+          setCountryData(data);
+        });
+    }
+    fetchCountryData();
+  }, []);
+
+  return (
+    <ProductTable
+        countryData = {countryData}
+      />
+  );
 }
 
 export default App;
